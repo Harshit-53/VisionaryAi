56032792b6cdafa439012f21403c8bfde6c6c10e
Harshit-53 <harshitsuryawanshi7233@gmail.com>
2025-07-20 22:10:55 +0530

    small ui commit

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import { stepsData } from '../assets/assets'
-import { motion } from 'framer-motion'
+import React from "react";
+import { stepsData } from "../assets/assets";
+import { motion } from "framer-motion";
 
 const Steps = () => {
-
-
   return (
     <motion.div
       className="flex flex-col items-center justify-center my-32"
@@ -13,30 +11,27 @@ const Steps = () => {
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
     >
-
-
-      <h1 className="text-3xl sm:text-4xl font-semibold mb-2">
-        How it works
-      </h1>
+      <h1 className="text-3xl sm:text-4xl font-semibold mb-2">How it works</h1>
       <p className="text-lg text-gray-600 mb-8">
         Transform Words Into Stunning Images
       </p>
 
-      <div className="space-y-4 w-full max-w-3xl text-sm" >
+      <div className="space-y-4 w-full max-w-3xl text-sm">
         {stepsData.map((item, index) => (
-          <div key={index} className="flex items-center gap-4 p-5 px-8 bg-white/20 rounded-lg shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300" >
+          <div
+            key={index}
+            className="flex items-center gap-4 p-5 px-8 bg-white/20 rounded-lg shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300"
+          >
             <img width={40} src={item.icon} alt="" />
             <div>
               <h2 className="text-xl font-medium">{item.title}</h2>
-              <p className="text-gray-500">
-                {item.description}
-              </p>
+              <p className="text-gray-500">{item.description}</p>
             </div>
           </div>
         ))}
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Steps
+export default Steps;
